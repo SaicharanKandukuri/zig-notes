cf9a30c0e9c981c8f896dd24b64e55ae3de4ed1a
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-02-15 23:51:22 +0530

    feat: Add working elements

--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,53 @@
 const std = @import("std");
 const capy = @import("capy");
 
+pub usingnamespace capy.cross_platform;
+var Aw: capy.Window = undefined;
 
 pub fn main() !void {
-
+    // Capy init for backend
     try capy.backend.init();
-    var window = try capy.Window.init();
+    // Create window
+    Aw = try capy.Window.init();
 
-    // try window.set(capy.Tabs(
-    //     .{
-    //         capy.Tab(
-    //             .{ .label = "Tab 2" },
-    //             capy.Column(
-    //                 .{}, .{
-    //                     capy.Button(.{ .label = "Button 1" }),
-    //                     capy.Button(.{ .label = "Button 2" }),
-    //                     capy.Button(.{ .label = "Button 3" }),
-    //                 }
-    //             )
-    //         )
-    //     }
-    // ));
-
-    try window.set(
-        capy.Row(
+    // setup for texts in container
+    try Aw.set(
+        capy.Column(
+            //  Column props
             .{},
+            // Column children
             .{
-                
+                capy.Label(.{ .text = "Note 📝" }),
+                capy.TextField(.{ .name="the feild" , .text = "..." }),
+                capy.Align(.{ .x = 1 }, capy.Button(.{ .label = "Add ➕", .onclick = stdLog})),
             }
-        )
+        ),
     );
 
-    window.resize(800, 600);
-    window.show();
+    Aw.setTitle("Zig-Notes (DEV)");
+    Aw.resize(400, 400);
+    Aw.show();
+
+    // Capy main loop
     capy.runEventLoop();
+}
+
+fn stdLog(widget: *anyopaque) !void {
+    const button = @ptrCast(
+        *capy.Button_Impl,
+        @alignCast(
+            @alignOf(capy.Button_Impl),
+            widget,
+        ),
+    );
+
+    const parent = button.getParent().?.getParent().?.as(capy.Container_Impl);
+    //                    ^~~~~~~~      ^~~~~~~~~~~
+    //                     (Align)       (Column)
+    const text = parent.getChildAs(capy.TextField_Impl, "the feild").?;
+    //                             ^~~~~~~~~~~~~~~~~~~   ^~~~~~~~~
+    //                              (TextField <type>)          (name)
+
+    std.debug.print("Button clicked: {s}\n", .{text.get("text")});
+    // finally get text from text field
 }
